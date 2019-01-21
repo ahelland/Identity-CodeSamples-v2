e1e485457370d8d64d3d1640017d58eda839db69
Andreas Helland <ahelland@users.noreply.github.com>
2019-01-22 00:11:21 +0100

    Added aad-b2c-custom_policies-dotnet-core

---
+++ aad-b2c-custom_policies-dotnet-core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aad_b2c_custom_policies_dotnet_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ aad-b2c-custom_policies-dotnet-core/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aad_b2c_custom_policies_dotnet_core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
