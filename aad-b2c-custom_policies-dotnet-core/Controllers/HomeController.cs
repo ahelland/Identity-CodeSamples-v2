5f393938432c56819e9987d88ad2d01690367ecf
ahelland <andreas@mobilitydojo.net>
2020-02-12 20:16:54 +0100

    Added Magic Link SignIn.

--- aad-b2c-custom_policies-dotnet-core/Controllers/HomeController.cs
+++ aad-b2c-custom_policies-dotnet-core/Controllers/HomeController.cs
@@ -2,11 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using aad_b2c_custom_policies_dotnet_core.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Configuration;
 
 namespace B2CNETCoreWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public IActionResult Index()
         {
             return View();
@@ -42,6 +51,17 @@ namespace B2CNETCoreWebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Separate SignIn handler for magic links sent by email
+        public IActionResult SignInLink(string id_token_hint)
+        {
+            var magic_link_auth = new AuthenticationProperties { RedirectUri = "/" };
+            magic_link_auth.Items.Add("id_token_hint", id_token_hint);
+
+            string magic_link_policy = Configuration.GetSection("AzureAdB2C")["MagicLinkPolicyId"];
+
+            return this.Challenge(magic_link_auth, magic_link_policy);
         }
     }
 }
--- aad-b2c-custom_policies-dotnet-core/Startup.cs
+++ aad-b2c-custom_policies-dotnet-core/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace aad_b2c_custom_policies_dotnet_core
 {
@@ -28,8 +32,13 @@ namespace aad_b2c_custom_policies_dotnet_core
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            //"Regular" AAD B2C policies
             services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
                 .AddAzureADB2C(options => Configuration.Bind("AzureADB2C", options));
+
+            //Magic link auth
+            string magic_link_policy = Configuration.GetSection("AzureAdB2C")["MagicLinkPolicyId"];
+            services.AddAuthentication(options => options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme).AddOpenIdConnect(magic_link_policy, GetOpenIdSignUpOptions(magic_link_policy)).AddCookie();
 
             services.AddAuthorization(options =>
             {
@@ -75,5 +84,32 @@ namespace aad_b2c_custom_policies_dotnet_core
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        private Action<OpenIdConnectOptions> GetOpenIdSignUpOptions(string policy)
+            => options =>
+            {
+                string clientId = Configuration.GetSection("AzureAdB2C")["ClientId"];
+                string B2CDomain = Configuration.GetSection("AzureAdB2C")["B2CDomain"];
+                string Domain = Configuration.GetSection("AzureAdB2C")["Domain"];
+
+
+                options.MetadataAddress = $"https://{B2CDomain}/{Domain}/{policy}/v2.0/.well-known/openid-configuration";
+                options.ClientId = clientId;
+                options.ResponseType = OpenIdConnectResponseType.IdToken;
+                options.SignedOutCallbackPath = "/signout/" + policy;
+                options.CallbackPath = "/signin-oidc-link";
+                options.SignedOutRedirectUri = "/";
+                options.SignInScheme = "AzureADB2C";
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnRedirectToIdentityProvider = context =>
+                    {
+                        if (context.Properties.Items.ContainsKey("id_token_hint"))
+                            context.ProtocolMessage.SetParameter("id_token_hint", context.Properties.Items["id_token_hint"]);                        
+
+                        return Task.FromResult(0);
+                    }
+                };
+            };
     }
 }
