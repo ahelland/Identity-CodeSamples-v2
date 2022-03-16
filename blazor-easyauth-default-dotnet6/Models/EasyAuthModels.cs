1b40d89aa807a5f55a7e2e625ce2a737e27e5fce
ahelland <ahelland@users.noreply.github.com>
2022-03-16 08:41:02 +0100

    Added claims readout to blazor-easyauth-default-dotnet6

---
+++ blazor-easyauth-default-dotnet6/Models/EasyAuthModels.cs
@@ -0,0 +1,19 @@
+﻿namespace blazor_easyauth_default_dotnet6.Models
+{
+    public class UserClaim
+    {
+        public string typ { get; set; }
+        public string val { get; set; }
+    }
+
+    public class Tokens
+    {
+        public string access_token { get; set; }
+        public DateTime expires_on { get; set; }
+        public string id_token { get; set; }
+        public string provider_name { get; set; }
+        public string refresh_token { get; set; }
+        public List<UserClaim> user_claims { get; set; }
+        public string user_id { get; set; }
+    }
+}
