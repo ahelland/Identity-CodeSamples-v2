1789a37c1f8073e6d91b1c55f114db85cdfff31c
ahelland <ahelland@users.noreply.github.com>
2026-09-08 18:25:54 +0200

    Added blazor_jwt_validator_dotnet10

---
+++ blazor_jwt_validator_dotnet10/Pages/Error.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace blazor_jwt_validator_dotnet10.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
